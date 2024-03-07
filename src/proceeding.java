d8d43d7cf2c9eb51b59d1f6bbaaf4097e3f5c361
Abdulahi Jama <kararjaamac1@gmail.com>
2024-03-07 02:13:59 -0800

    if statements

---
+++ src/proceeding.java
@@ -0,0 +1,20 @@
+public class proceeding {
+    public static void main(String[] args) {
+
+        // proceeding operational
+
+
+        System.out.println(!(false) || true && false);
+        boolean bool = !(1 <8 && (5>2 || 3 < 5));
+
+        System.out.println(bool);
+
+
+    }
+}
+
+
+
+
+
+
---
+++ src/variabales/dataType.java
@@ -0,0 +1,15 @@
+package variabales;
+
+public class dataType {
+    public static void main(String[] args) {
+
+        String firstName= "sadeeq";
+        String lastName= "hasan";
+
+        System.out.println("my first name is " + lastName + " and my last name is " + firstName);
+
+
+
+
+    }
+}
