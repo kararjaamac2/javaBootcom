256cece0e73df8fc81ed10e1b89f33fa778f113e
Abdulahi Jama <kararjaamac1@gmail.com>
2024-03-07 02:02:38 -0800

    if statements

---
+++ src/IfStatement.java
@@ -0,0 +1,36 @@
+public class IfStatement {
+
+    public static void main(String[] args) {
+        if (9 > 2) {
+            System.out.println("control the flow rock");
+
+        }
+        int apple = 10;
+        int orange = 5;
+        if (apple > orange) {
+            System.out.println("I have more apple than orange");
+        }
+        String userName = "abc";
+        if (userName.equals(userName)) {
+            System.out.println("Access granted");
+            System.out.println("welcome to agent ");
+        }
+        int books = 5;
+        if (books < 10) {
+            books += 10;
+            System.out.println(books);
+            boolean hot = true;
+            if (hot==true){
+                System.out.println(" can we get some ice");
+
+                int age = 10;
+                if (age >= 18){
+                    System.out.println("Adult");
+
+                }
+
+            }
+        }
+    }
+
+}
